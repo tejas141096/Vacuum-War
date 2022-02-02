ae6052bdb5455612ad54b0f71af6f9b0c7369c58
Junce Wang <u1330351@utah.edu>
2022-02-02 16:57:00 -0700

    Add Auto Reset Head to Empty (By Lei on Junce's PC)

--- vaccum_war/Assets/Scripts/VacuumGun/VacuumGunRFID.cs
+++ vaccum_war/Assets/Scripts/VacuumGun/VacuumGunRFID.cs
@@ -9,6 +9,9 @@ namespace VWPrototype
     {
         // Start is called before the first frame update
         public SerializableDictionary<string,HeadMode> UIDList;
+        public bool AutoResetHead = false;
+        public int SecondsToResetHead = 3;
+        private Coroutine resetTimer = null;
         new void Start()
         {
             base.Start();
@@ -28,11 +31,26 @@ namespace VWPrototype
                 //sb.AppendFormat("(#{0}={1})    ", b, (char)b);
             Debug.Log("Received Card UID: " + sb);
             SwitchHeadByUid(sb.ToString());
+            if (AutoResetHead) {
+                if (resetTimer != null)
+                {
+                    StopCoroutine(resetTimer);
+                }
+                resetTimer = StartCoroutine(ResetHead(SecondsToResetHead));
+            }
+        }
+
+        IEnumerator ResetHead(int time) {
+            yield return new WaitForSecondsRealtime(time);
+            Debug.Log("Reset Head to Empty");
+            gunFrame.SwitchHead(HeadMode.Empty);
+            resetTimer = null;
         }
 
         public void SwitchHeadByUid(string uid) {
             HeadMode newMode;
             if (UIDList.TryGetValue(uid,out newMode)) {
+                Debug.Log($"Switch Head to {newMode.ToString()}");
                 gunFrame.SwitchHead(newMode);
             }
         }
