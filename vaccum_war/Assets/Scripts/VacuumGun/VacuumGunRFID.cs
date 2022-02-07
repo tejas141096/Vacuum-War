5d03c41a668842942b2e3e2175cb9d1123a2d382
ZESHI CHEN <48173949+Bolicrad@users.noreply.github.com>
2022-02-06 23:55:01 -0700

    Add automatic Bluetooth serial ports detection and auto connection
    我创死你

--- vaccum_war/Assets/Scripts/VacuumGun/VacuumGunRFID.cs
+++ vaccum_war/Assets/Scripts/VacuumGun/VacuumGunRFID.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using UnityEngine;
 
@@ -12,16 +13,42 @@ namespace VWPrototype
         public bool AutoResetHead = false;
         public float SecondsToResetHead = 1.5f;
         private Coroutine resetTimer = null;
+        public SerialControllerCustomDelimiter serialController;
         new void Start()
         {
             base.Start();
-
+            Open();
         }
 
         // Update is called once per frame
         void Update()
         {
 
+        }
+
+        private void Open(string arguments = null)
+        {
+            System.Diagnostics.Process p = new System.Diagnostics.Process();
+            //设置.net的程序路径
+            p.StartInfo.FileName = Application.streamingAssetsPath + "/GetSerialPorts.exe";
+            p.StartInfo.Arguments = arguments;
+            p.StartInfo.CreateNoWindow = true;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.StandardOutputEncoding = Encoding.Default;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            p.Start();
+            StreamReader s = p.StandardOutput;
+            p.WaitForExit();
+            Manager(s.ReadToEnd());
+            s.Close();
+        }
+        private void Manager(string content)
+        {
+            //处理接收到的内容
+            Debug.Log(content);
+            serialController.portName = content;
+            serialController.gameObject.SetActive(true);
         }
 
         void OnMessageArrived(byte[]message) {
